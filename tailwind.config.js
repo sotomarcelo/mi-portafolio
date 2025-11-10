7f40343799f3a3798ac5ba9d4f110a0389032d39
Marcelo Soto <marcelo.soto@innovex.cl>
2025-11-10 15:46:23 -0300

    Se crea proyecto inicial

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}', './node_modules/flowbite/**/*.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('flowbite/plugin')],
+};
