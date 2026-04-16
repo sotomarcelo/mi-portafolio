f1b54b6d5ef79fc54fe613ac347e8de5022b788c
sotomarcelo <marcelo.soto.barria99@gmail.com>
2026-04-16 15:34:52 -0400

    feat: update Astro configuration and package dependencies
    - Set output format to 'static' in astro.config.mjs.
    - Specify Node.js engine requirement in package.json.
    - Update pnpm-lock.yaml to include new dependencies from Vercel.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 import icon from 'astro-icon';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: vercel(),
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
