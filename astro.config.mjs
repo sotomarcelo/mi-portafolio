2e278a5101a309460f3bae7288b5431a4dd40e8e
sotomarcelo <marcelo.soto.barria99@gmail.com>
2026-04-16 15:37:56 -0400

    se añade todo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 import icon from 'astro-icon';
 
 import tailwindcss from '@tailwindcss/vite';
@@ -9,7 +8,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: vercel(),
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
